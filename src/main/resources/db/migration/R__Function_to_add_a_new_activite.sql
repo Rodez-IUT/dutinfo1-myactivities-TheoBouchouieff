dbb6ef5aa4a1d25b77971a21425e5c1a28ae8e17
Théo le collabo <theo.bouchouieff@iut-rodez.fr>
2019-02-20 16:26:37 +0100

    function to add new activity OK

--- src/main/resources/db/migration/R__Function_to_add_a_new_activite.sql
+++ src/main/resources/db/migration/R__Function_to_add_a_new_activite.sql
@@ -1,5 +1,5 @@
-CREATE OR REPLACE FUNCTION add_activities_with_title(title varchar(200)) RETURNS bigint AS $$
+CREATE OR REPLACE FUNCTION add_activity_with_title(title varchar(200)) RETURNS bigint AS $$
 	INSERT INTO activity(id, title)
-	VALUES (nextval('id_generator'), add_activities_with_title.title)
+	VALUES (nextval('id_generator'), add_activity_with_title.title)
 	returning id;
 $$ LANGUAGE SQL;
